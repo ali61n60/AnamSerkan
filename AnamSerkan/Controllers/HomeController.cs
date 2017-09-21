dcb0f88c4efd5d2e5dfd4d85b57d524a82473489
Ali <ali62n62@yahoo.com>
2017-09-21 12:39:14 +0330

    equvalent page added

--- AnamSerkan/Controllers/HomeController.cs
+++ AnamSerkan/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AnamSerkan.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        
         public IActionResult Index()
         {
             return View();
