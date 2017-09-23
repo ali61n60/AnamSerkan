71d445ddbb707b69503c127b365e41a10454f141
ali2 <ali62n62@yahoo.com>
2017-09-23 11:13:25 +0330

    creating Product view component

--- AnamSerkan/Components/ProductViewComponent.cs
+++ AnamSerkan/Components/ProductViewComponent.cs
@@ -16,7 +16,7 @@ namespace AnamSerkan.Components
             //    .Select(x => x.Category)
             //    .Distinct()
             //    .OrderBy(x => x));
-            return null;
+            return View();
         }
     }
 }
