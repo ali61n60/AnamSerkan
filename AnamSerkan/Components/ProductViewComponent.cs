7fe03f29366eb53857f459796388ffc5c2af76db
ali2 <ali62n62@yahoo.com>
2017-09-23 10:43:30 +0330

    creating view component

---
+++ AnamSerkan/Components/ProductViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnamSerkan.Components
+{
+    public class ProductViewComponent:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            //ViewBag.SelectedCategory = RouteData?.Values["category"];
+            //return View(repository.GetProducts()
+            //    .Select(x => x.Category)
+            //    .Distinct()
+            //    .OrderBy(x => x));
+            return null;
+        }
+    }
+}
---
+++ AnamSerkan/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnamSerkan.Models
+{
+    public class Product
+    {
+        public string ImagePath { get; set; }
+        public string ProductName { get; set; }
+    }
+}
